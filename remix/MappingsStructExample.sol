add964b002006412618f894730c14b871101504c
Iker Macaya Faber <42570380+Maciker@users.noreply.github.com>
2020-10-25 09:52:11 +0100

    Add files via upload

---
+++ remix/MappingsStructExample.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.5.17;
+
+contract MappingsStructExample {
+    
+    struct Payment {
+        uint amount;
+        uint timestamps;
+    }
+    
+    struct Balance {
+        uint totalBalance;
+        uint numPayments;
+        mapping(uint => Payment) payments;
+    }
+    
+    mapping(address => Balance) public balanceRecieved;
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+    
+    function sendMoney() public payable {
+        balanceRecieved[msg.sender].totalBalance += msg.value;
+        
+        Payment memory payment = Payment(msg.value, now);
+        
+        balanceRecieved[msg.sender].payments[balanceRecieved[msg.sender].numPayments] = payment;
+        balanceRecieved[msg.sender].numPayments++;
+    }
+    
+    function withDrawMoney(address payable _to, uint _amount) public {
+        require(balanceRecieved[msg.sender].totalBalance >= _amount, 'Not Enough funds');
+        balanceRecieved[msg.sender].totalBalance -= _amount;
+        _to.transfer(_amount);
+    }
+    
+    function withDrawAllMoney(address payable _to) public {
+        uint balanceToSend = balanceRecieved[msg.sender].totalBalance;
+        balanceRecieved[msg.sender].totalBalance = 0;
+        _to.transfer(balanceToSend);   
+    }
+}
